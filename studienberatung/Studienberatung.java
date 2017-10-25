ab33463fd6866e07f2595c17a5e13035a552bacd
Ten <snorka@live.de>
2017-10-25 11:00:28 +0200

    added bist-du-wissbegierig-question

--- studienberatung/Studienberatung.java
+++ studienberatung/Studienberatung.java
@@ -7,6 +7,15 @@ public class Studienberatung {
 	public void main(){
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Hast du Abitur?");
+		
+		String answer = scanner.next();
+		if(answer.equalsIgnoreCase("j")){
+			System.out.println("Bist du wissbegierig?");
+			answer = scanner.next();
+			if(answer.equalsIgnoreCase("n")){
+				
+			}
+		}
 	}
 
 	public void start(){
