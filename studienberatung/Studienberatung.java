0e5462d64273fc76e060186f421beb2bbc949c45
Ten <snorka@live.de>
2017-10-25 11:11:12 +0200

    added remaining stuff

--- studienberatung/Studienberatung.java
+++ studienberatung/Studienberatung.java
@@ -15,12 +15,28 @@ public class Studienberatung {
 			System.out.println("Hast du Fachabitur?");
 			answer = scanner.next();
 			
-		}else if(answer.equalsIgnoreCase("j")){
+			if(answer.equalsIgnoreCase("n")){
+				System.out.println("Hast du eine Berufsausbildung?");
+				answer = scanner.next();
+					
+				if(answer.equalsIgnoreCase("n")){
+					System.out.println("Besser noch studieren.");
+				}else{
+					System.out.println("Du kannst ja später noch studieren.");
+				}
+				
+			}else{
+				System.out.println("Nicht studieren.");
+			}
+			
+		}else{
 			System.out.println("Bist du wissbegierig?");
 			answer = scanner.next();
 			
 			if(answer.equalsIgnoreCase("n")){
-				
+				System.out.println("Nicht studieren.");
+			}else{
+				System.out.println("Auf jeden Fall studieren.");
 			}
 		}
 	}
