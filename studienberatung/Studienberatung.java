9d5e1b683c7c3e92aeff7f328064d4e53eb61f68
Ten <snorka@live.de>
2017-10-25 10:46:07 +0200

    Implemented Scanner

--- studienberatung/Studienberatung.java
+++ studienberatung/Studienberatung.java
@@ -1,5 +1,18 @@
 package studienberatung;
 
-public class Studienberatung {
+import java.util.Scanner;
 
+public class Studienberatung {
+	
+	public void main(){
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Hast du Abitur?");
+	}
+
+	public void start(){
+	}
+	
+	public void getAnswer(){
+		
+	}
 }
