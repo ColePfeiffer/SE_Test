5c057f0c2e840d05a2a61f6be7763bcb5939bfa1
Ten <snorka@live.de>
2017-10-25 10:54:10 +0200

    added hast du fachabitur-question

--- studienberatung/Studienberatung.java
+++ studienberatung/Studienberatung.java
@@ -7,6 +7,14 @@ public class Studienberatung {
 	public void main(){
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Hast du Abitur?");
+		
+		String answer = scanner.next();
+		if(answer.equalsIgnoreCase("n")){
+			System.out.println("Hast du Fachabitur?");
+			answer = scanner.next();
+		}else{
+			
+		}
 	}
 
 	public void start(){
